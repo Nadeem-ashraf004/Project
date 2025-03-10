2c43af63830cf14eb9303b0714c2615d25a08e63
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-03-10 21:35:35 +0500

    A

--- MeanMedianMode.py
+++ MeanMedianMode.py
@@ -2,5 +2,15 @@ import numpy as np
 from scipy import stats
 marks=[]
 def manu():
-    global marks
     
+    print("Memu :")
+    print("A . To Enter your Subject marks :")
+    print("B ./To Calculate Mean :")
+    print("C./ To Calculate Mode :")
+    print("D./ To calculate variance :")
+    print("X./ To exit the application ")
+def all_marks():
+    global marks
+    student_marks=int(input("Your Marks  :"))
+    marks=list(map(int ,student_marks.split(",")))
+    print(f"your obtaine marks : {marks}")
