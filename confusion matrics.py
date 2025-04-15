719391f6d9eeedb0a18c80ee5336e3a525ff0c9d
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-04-15 14:03:37 +0500

    confusion matics

--- confusion matrics.py
+++ confusion matrics.py
@@ -7,3 +7,4 @@ confusion_matrix=matrics.confusion_matrix(actual,predict)
 cm_display=matrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,cm_display_lable=[0,1])
 cm_display.plot()
 plt.show
+##
